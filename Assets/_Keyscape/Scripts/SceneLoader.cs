cfbfb74b423595330fe2ae1c67ea3c2c012442a4
leomani3 <leoveyssiere.pro@hotmail.fr>
2021-01-30 00:22:55 +0100

    Moved everything in a _Keyscape folder

---
+++ Assets/_Keyscape/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+
+    public TransitionScreen transitionScreen;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadSceneAsync(int sceneToLoadIndex)
+    {
+        StartCoroutine(LoadSceneAsyncCoroutine(sceneToLoadIndex));
+    }
+
+    private IEnumerator LoadSceneAsyncCoroutine(int sceneToLoadIndex)
+    {
+        //Start fade in
+        yield return new WaitForSeconds(transitionScreen.StartIn());
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoadIndex);
+        asyncLoad.allowSceneActivation = true;
+
+        while (asyncLoad.isDone == false)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(.4f);
+
+        transitionScreen.StartOut();
+    }
+}
