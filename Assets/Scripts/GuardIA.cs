f05a3f67be52efc730dd4bd23aa160fbfdf70705
leomani3 <leoveyssiere.pro@hotmail.fr>
2021-01-30 00:15:10 +0100

    CameraController

--- Assets/Scripts/GuardIA.cs
+++ Assets/Scripts/GuardIA.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MyBox;
@@ -5,4 +6,5 @@ using UnityEngine;
 
 public class GuardIA : MonoBehaviour
 {
+    
 }
---
+++ Assets/Scripts/Singletons/CameraController.cs
@@ -0,0 +1,39 @@
+using System;
+using Cinemachine;
+using DG.Tweening;
+using HomaGames.Internal.Utilities;
+using System.Collections;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance;
+    
+    [SerializeField] CinemachineMixingCamera mixingCamera;
+    [SerializeField] float changeDuration;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetCamera(int index) => SetCamera(index, changeDuration);
+
+    public void SetCamera(int index, float time)
+    {
+        if (!isActiveAndEnabled)
+            return;
+        for (int i = 0; i < mixingCamera.ChildCameras.Length; i++)
+        {
+            bool active;
+            active = i == index;
+            DOTween.To(() => mixingCamera.GetWeight(i), v => mixingCamera.SetWeight(i, v), active ? 1.0f : 0.0f, time).SetEase(Ease.InOutSine);
+        }
+    }
+
+    public float ChangeDuration
+    {
+        set => changeDuration = value;
+    }
+}
