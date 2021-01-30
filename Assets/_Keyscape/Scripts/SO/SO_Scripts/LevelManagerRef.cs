e7af145d50b4210d38b33c1c87b6274739686d63
leomani3 <leoveyssiere.pro@hotmail.fr>
2021-01-30 18:17:48 +0100

    LevelManager : getNearestPosition

---
+++ Assets/_Keyscape/Scripts/SO/SO_Scripts/LevelManagerRef.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelManagerRef", menuName = "ScriptableObjects/LevelManagerRef", order = 1)]
+public class LevelManagerRef : ScriptableObject
+{
+    public LevelManager levelManager;
+}
