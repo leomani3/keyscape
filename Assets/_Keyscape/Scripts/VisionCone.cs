c348a1067cb6777bd7cd908fd3a9e7ff4b998f26
leomani3 <leoveyssiere.pro@hotmail.fr>
2021-01-30 13:04:01 +0100

    Vision cone finished

--- Assets/_Keyscape/Scripts/VisionCone.cs
+++ Assets/_Keyscape/Scripts/VisionCone.cs
@@ -21,10 +21,13 @@ public class VisionCone : MonoBehaviour
     [SerializeField] private int _indexPoints;
     [SerializeField] private int _indexTris;
 
+    private RaycastHit _hit;
+
     private void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
         _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer.material = material;
     }
 
     private void Update()
@@ -36,25 +39,23 @@ public class VisionCone : MonoBehaviour
         _indexPoints = 0;
         _indexTris = 0;
         
-        _points[0] = transform.position;
+        _points[0] = transform.parent.InverseTransformPoint(transform.parent.position);
         _indexPoints++;
 
         if (rayNumber - 1 != 0)
             _angleStep = coneAngle / (rayNumber - 1);
         else
             _angleStep = coneAngle;
-
-
-        //Vector3 endRayPoint = new Vector3(transform.position.x + raycastDistance * Mathf.Cos((coneAngle / 2 + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad), transform.position.y, transform.position.z + raycastDistance * Mathf.Sin((coneAngle / 2 + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad));
-        //_points[_indexPoints] = endRayPoint;
-        //_indexPoints++;
-        //Debug.DrawLine(transform.position, endRayPoint);
+        
 
         for (int i = 0; i < rayNumber; i++)
         {
-            //Physics.Raycast(transform.position, endRayPoint - transform.position, raycastDistance);
             Vector3 rayPoint = new Vector3(transform.position.x + raycastDistance * Mathf.Cos((-coneAngle / 2 + (_angleStep * i) + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad), transform.position.y, transform.position.z + raycastDistance * Mathf.Sin((-coneAngle / 2 + (_angleStep * i) + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad));
-            _points[_indexPoints] = rayPoint;
+            if (Physics.Raycast(transform.position, rayPoint - transform.position, out _hit, raycastDistance))
+            {
+                rayPoint = _hit.point;
+            }
+            _points[_indexPoints] = transform.parent.InverseTransformPoint(rayPoint);
             _indexPoints++;
 
             if (i > 0)
@@ -64,16 +65,11 @@ public class VisionCone : MonoBehaviour
                 _tris[_indexTris + 2] = i;
                 _indexTris += 3;
             }
-            Debug.DrawLine(transform.position, rayPoint);
         }
-        
-        //Vector3 endRayPoint2 = new Vector3(transform.position.x + raycastDistance * Mathf.Cos((-coneAngle / 2 + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad), transform.position.y, transform.position.z + raycastDistance * Mathf.Sin((-coneAngle / 2 + 90  - transform.eulerAngles.y) * Mathf.Deg2Rad));
-        //_points[_indexPoints] = endRayPoint2;
-        //_indexPoints++;
-        //Debug.DrawLine(transform.position, endRayPoint2);
 
         _mesh.vertices = _points;
         _mesh.triangles= _tris;
+        _meshFilter.transform.position = transform.parent.position;
         _meshFilter.mesh = _mesh;
     }
 }
