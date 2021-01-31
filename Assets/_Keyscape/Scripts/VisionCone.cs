28486d6b1979ec49bea54208570ceb9b984df69e
leomani3 <leoveyssiere.pro@hotmail.fr>
2021-01-31 15:36:23 +0100

    vision cone fixed

--- Assets/_Keyscape/Scripts/VisionCone.cs
+++ Assets/_Keyscape/Scripts/VisionCone.cs
@@ -53,8 +53,8 @@ public class VisionCone : MonoBehaviour
         {
             
             Vector3 rayPoint = new Vector3(transform.position.x + raycastDistance * Mathf.Cos((-coneAngle / 2 + (_angleStep * i) + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad), transform.position.y , transform.position.z + raycastDistance * Mathf.Sin((-coneAngle / 2 + (_angleStep * i) + 90 - transform.eulerAngles.y) * Mathf.Deg2Rad));
-            Ray ray = new Ray(transform.position - new Vector3(0, transform.lossyScale.y, 0), rayPoint - transform.position);
-            //Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.magenta);
+            Ray ray = new Ray(transform.parent.position, rayPoint - transform.parent.position);
+            //Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.cyan);
             if (Physics.Raycast(ray, out _hit, raycastDistance, ~IgnoreLayer))
             {
                 PickUpItem item = _hit.collider.GetComponent<PickUpItem>();
@@ -64,7 +64,7 @@ public class VisionCone : MonoBehaviour
                 }
                 rayPoint = _hit.point;
             }
-            _points[_indexPoints] = transform.parent.InverseTransformPoint(new Vector3(rayPoint.x, transform.parent.position.y + 0.1f, rayPoint.z));
+            _points[_indexPoints] = transform.parent.InverseTransformPoint(new Vector3(rayPoint.x, transform.parent.position.y  + 0.1f, rayPoint.z));
             _indexPoints++;
 
             if (i > 0)
